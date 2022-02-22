7cb142ea008d5aa48f734eb95e0543d5e7430dd8
Michael Uno <michael2@michaeluno.jp>
2022-02-22 15:29:29 +0900

    Fix: Fix the wrong resource type for the CSS minifier class

--- source/Minifier/InlineCSSMinifier.php
+++ source/Minifier/InlineCSSMinifier.php
@@ -5,6 +5,15 @@ namespace AdminPageFrameworkCompiler\Minifier;
 include_once( __DIR__ . '/AbstractMinifier.php' );
 class InlineCSSMinifier extends AbstractMinifier {
 
-    public $sResourceType = 'js';
+    public $sResourceType = 'css';
+
+    /**
+     * @param  string $sResourceCode    CSS rules.
+     * @return string
+     * @since  1.0.0
+     */
+    public function getMinified( $sResourceCode ) {
+        return $sResourceCode;
+    }
 
 }
