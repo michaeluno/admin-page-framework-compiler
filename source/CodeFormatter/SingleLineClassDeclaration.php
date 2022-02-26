80a9e8127d8f07ff9d016682f1672eeb75200bc5
Michael Uno <michael2@michaeluno.jp>
2022-02-26 16:32:30 +0900

    Feature: Add the ability to format PHP code with single lines of class declarations

--- source/CodeFormatter/SingleLineClassDeclaration.php
+++ source/CodeFormatter/SingleLineClassDeclaration.php
@@ -14,7 +14,18 @@ class SingleLineClassDeclaration extends AbstractCodeFormatter {
      * @since  1.1.0
      */
     public function get( $sCode ) {
-        return $sCode;
+        $_sPattern = '/'    // opening delimiter
+            . '[^\S\r\n]\K' // a space is required to match
+            . '('           // $1
+                . '(((abstract|static|final)\s)+)?'    // scope and visibility keywords
+                . 'class\s+[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*+' // class + {class name}
+                // not implemented at the moment as there are multiple interfaces and return types in PHP 8 can be set and these will be really complicated to match including supporting multiple lines
+                //  . '(\s+(extends|implements)\s+[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*+)?'
+            . ')'
+            // . '[\s\r\n]+' . '{'  // not implemented
+            . '/'  // closing delimiter
+            ;
+        return preg_replace( $_sPattern, PHP_EOL . '$1', $sCode );
     }
 
 }
