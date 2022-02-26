8963b0c3673a1d837aa1f34d5ff95ae4eae4e4ac
Michael Uno <michael2@michaeluno.jp>
2022-02-26 17:12:47 +0900

    Fix: Fix a bug that a class declaration line get separated to multiple lines

--- source/CodeFormatter/SingleLineClassDeclaration.php
+++ source/CodeFormatter/SingleLineClassDeclaration.php
@@ -15,9 +15,10 @@ class SingleLineClassDeclaration extends AbstractCodeFormatter {
      */
     public function get( $sCode ) {
         $_sPattern = '/'    // opening delimiter
-            . '[^\S\r\n]\K' // a space is required to match
+            // . '[^\S\r\n]\K' // a white space is required to match
+            . '([\s\r\n])' // a white space or a line break is required to match
             . '('           // $1
-                . '(((abstract|static|final)\s)+)?'    // scope and visibility keywords
+                . '(((abstract|static|final)\s+)+)?'    // scope and visibility keywords
                 . 'class\s+[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*+' // class + {class name}
                 // not implemented at the moment as there are multiple interfaces and return types in PHP 8 can be set and these will be really complicated to match including supporting multiple lines
                 //  . '(\s+(extends|implements)\s+[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*+)?'
@@ -25,7 +26,7 @@ class SingleLineClassDeclaration extends AbstractCodeFormatter {
             // . '[\s\r\n]+' . '{'  // not implemented
             . '/'  // closing delimiter
             ;
-        return preg_replace( $_sPattern, PHP_EOL . '$1', $sCode );
+        return preg_replace( $_sPattern, PHP_EOL . '$2', $sCode );
     }
 
 }
